01736f02b740ac012732028e8f9fe99ebc117170
ArduBrico <ardubrico@hotmail.com>
2023-11-16 11:05:51 +0100

    Actualizado Pulsadores funciona sin wifi

--- MQTT/Persianas_Porcentaje/Persianas_Con_Pulsadores/Persianas_Con_Pulsadores.ino
+++ MQTT/Persianas_Porcentaje/Persianas_Con_Pulsadores/Persianas_Con_Pulsadores.ino
@@ -1,21 +1,31 @@
-//// https://www.youtube.com/@ArduBrico ////
+///// https://www.youtube.com/@ArduBrico ////
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoOTA.h>
 
-const char* ssid = "SSID";                 //Nombre de tu SSID
-const char* password = "PASSWORD";         //Contraseña de tu SSID
-const char* mqtt_server = "XXX.XXX.X.XX";  //I.P de tu servidor MQTT
-int mqttport = 1883;
-const char* mqttusuario = "MQTT";       // Usuario MQTT en Home Assistant
-const char* mqttpass = "PASS_MQTT";     // Contraseña para el usuario MQTT en Home Assistant
-const char* OTA_password = "PASS_OTA";  // Contraseña OTA
-#define CLIENT_ID "Persiana_Sala"       //debe ser único en tu sistema
-#define MQTT_TOPIC "persianas/sala"     //debe que ser el mismo que tengas en configuration.yaml
+///// PARAMETROS CONFIGURABLES /////
 
-unsigned long periodo_subir = 30000;  // tiempo que tarda la persiana en subir (segundos * 1000)
-unsigned long periodo_bajar = 30000;  // tiempo que tarda la persiana en bajar (segundos * 1000)
+const char* ssid = "SSID";                 // Nombre de tu SSID
+const char* password = "PASSWORD SSID";    // Contraseña de tu SSID
+const char* mqtt_server = "XXX.XXX.X.XX";  // I.P de tu servidor MQTT
+int mqttport = 1883;                       // Puerto para MQTT
+const char* mqttusuario = "MQTT";          // Usuario MQTT en Home Assistant
+const char* mqttpass = "PASS_MQTT";        // Contraseña para el usuario MQTT en Home Assistant
+const char* OTA_password = "PASS_OTA";     // Contraseña OTA
+#define CLIENT_ID "Persiana_Sala"           // debe ser único en tu sistema
+#define MQTT_TOPIC "persianas/sala"  // debe que ser el mismo que tengas en configuration.yaml
+
+const int relay1 = 13;    // Relé subir lo conectamos a D7
+const int relay2 = 12;    // Relé bajar lo conectamos a D6
+const int Pul_subir = 5;  // Pulsador subir lo conectamos a  D1
+const int Pul_bajar = 4;  // Pulsador bajar lo conectamos a  D2
+
+unsigned long periodo_subir = 15000;  // tiempo que tarda la persiana en subir
+unsigned long periodo_bajar = 15000;  // tiempo que tarda la persiana en bajar
+
+///// FIN DE PARAMETROS CONFIGURABLES /////
+
 unsigned long periodo3;
 unsigned long periodo4;
 byte temp_subir = 0;
@@ -32,12 +42,30 @@ int position_real;
 int position_str;
 int pos_str;
 int pos_real;
+int val1 = 0;
+int val2 = 0;
+int old_val1 = 0;
+int old_val2 = 0;
+int state = 0;
+int old_state = 0;
+int state2 = 0;
+int old_state2 = 0;
+unsigned long tiempo1;
+unsigned long tiempo2;
+unsigned long tiempomax1 = 100;
+unsigned long tiempomax2 = 100;
+bool flanco1 = false;
+bool flanco2 = false;
+bool flancowifi = true;
+bool flancomqtt = false;
+bool contador = false;
+int cont = 0;
+bool subiendo;
+bool bajando;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-const int relay1 = 13;  //Lo conectamos a D7
-const int relay2 = 12;  //Lo conectamos a D6
 const char* topic = MQTT_TOPIC "/comando";
 const char* topic_set = MQTT_TOPIC "/set_position";
 const char* topic_pos = MQTT_TOPIC "/position";
@@ -45,14 +73,14 @@ const char* topic_pos = MQTT_TOPIC "/position";
 void setup() {
   Serial.begin(9600);
   Serial.println("setup begin");
-  digitalWrite(relay1, HIGH);
-  digitalWrite(relay2, HIGH);
+  digitalWrite(relay1, HIGH);  // Iniciamoslos relés apagados, cambiar a LOW si funciona a la inversa
+  digitalWrite(relay2, HIGH);  // Iniciamoslos relés apagados, cambiar a LOW si funciona a la inversa
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
+  pinMode(Pul_subir, INPUT_PULLUP);  // Utilizamos la resistencia interna de la placa
+  pinMode(Pul_bajar, INPUT_PULLUP);  // Utilizamos la resistencia interna de la placa
   delay(10);
 
-  //inicia wifi y mqqt
-  setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 
@@ -80,28 +108,109 @@ void setup() {
 
   Serial.println("setup end");
 }
-
 void loop() {
   ArduinoOTA.handle();
 
-  if (WiFi.status() != WL_CONNECTED) {
+  ///// INTENTAMOS CONECTAR A WIFI, SI NO CONECTA, ESPERAMOS UN TIEMPO /////
+  if ((WiFi.status() != WL_CONNECTED) && (flancowifi == true)) {
     setup_wifi();
     reconnect();
     client.setServer(mqtt_server, mqttport);
     client.setCallback(callback);
   }
   if (!client.connected()) {
+    contador = true;
+    cont++;
+    delay(10);
+  }
+
+  if ((contador == true) && (cont == 1000)) {
+    flancomqtt = true;
+    flancowifi = true;
     reconnect();
   }
   client.loop();
 
+
+  ///// PULSADOR SUBIR ////
+
+  val1 = digitalRead(Pul_subir);
+
+  if (!val1 && old_val1) {
+    tiempo1 = millis();
+    flanco1 = true;
+  }
+  old_val1 = val1;
+
+  if (flanco1 && !val1)
+    if (millis() - tiempo1 >= tiempomax1) {
+      state = 1 - state;
+      flanco1 = false;
+    } else
+      state2 = state2;
+  else {
+    flanco1 = false;
+  }
+
+  ///// PULSADOR BAJAR ////
+
+  val2 = digitalRead(Pul_bajar);
+
+  if (!val2 && old_val2) {
+    tiempo2 = millis();
+    flanco2 = true;
+  }
+  old_val2 = val2;
+
+  if (flanco2 && !val2)
+    if (millis() - tiempo2 >= tiempomax2) {
+      state2 = 1 - state2;
+      flanco2 = false;
+    } else
+      state2 = state2;
+  else {
+    flanco2 = false;
+  }
+
+  ///// ACCION PULSADORES /////
+
+  if (state == 1 && old_state == 0 && bajando == true) {
+    parada();
+
+  } else if (state == 1 && old_state == 0 && bajando == false) {
+    position_str = 100;
+    subiendo = true;
+    bajando = false;
+
+  } else if (state == 0 && old_state == 1) {
+    parada();
+  }
+  old_state = state;
+
+  if (state2 == 1 && old_state2 == 0 && subiendo == true) {
+    parada();
+
+  } else if (state2 == 1 && old_state2 == 0 && subiendo == false) {
+    position_str = 0;
+    bajando = true;
+    subiendo = false;
+
+  } else if (state2 == 0 && old_state2 == 1) {
+    parada();
+  }
+
+  old_state2 = state2;
+
   ///// ACTIVAR CONTADOR PORCENTAJE ////
+
   if (temp_subir_p == 1 && (position_str > position_real)) {
     periodo3 = ((position_str - position_real) * periodo_subir / 100);
     pos_str = position_str;
     digitalWrite(relay1, LOW);
     digitalWrite(relay2, HIGH);
     Serial.println("SUBIENDO");
+    subiendo = true;
+    bajando = false;
     tiempoAnterior3 = millis();
     client.publish(MQTT_TOPIC "/estado", "subiendo");
     temp_bajar = 0;
@@ -115,6 +224,8 @@ void loop() {
     digitalWrite(relay1, HIGH);
     digitalWrite(relay2, LOW);
     Serial.println("BAJANDO");
+    subiendo = false;
+    bajando = true;
     tiempoAnterior4 = millis();
     client.publish(MQTT_TOPIC "/estado", "bajando");
     temp_bajar = 1;
@@ -142,8 +253,8 @@ void loop() {
     }
   }
 
-
   ///// CONTADOR BAJAR PORCENTAJE ////
+
   if (temp_bajar == 1) {
 
     currentMillis = millis();
@@ -162,7 +273,6 @@ void loop() {
   }
 }
 
-
 void setup_wifi() {
   delay(10);
   // Nos conectamos a la Red
@@ -172,20 +282,29 @@ void setup_wifi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    for (int i = 0; i <= 50; i++) {
-      delay(10);
-      if (i == 50) {
-        Serial.println(".");
-      } else {
-        Serial.print(".");
+  while ((WiFi.status() != WL_CONNECTED) && (flancowifi == true)) {  // Intenamos conectar a Wifi durante 5 segundos
+    for (int i = 0; i <= 5; i++) {
+      Serial.println(i);
+      delay(1000);
+      if (i == 5) {
+        flancowifi = false;
+        contador = true;
+        break;
       }
     }
   }
-  Serial.println("");
-  Serial.println("WiFi conectado");
-  Serial.println("Dirección IP: ");
-  Serial.println(WiFi.localIP());
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("No se ha podido conectar al Wifi, nuevo intento en unos segundos...");
+    flancowifi = false;
+  } else {
+    Serial.println("");
+    Serial.println("WiFi conectado");
+    Serial.println("Dirección IP: ");
+    Serial.println(WiFi.localIP());
+    cont = 0;
+    flancowifi = false;
+    contador = false;
+  }
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -230,31 +349,39 @@ void parada() {
   temp_subir = 0;
   temp_subir_p = 1;
   temp_bajar_p = 1;
+  state = 0;
+  old_state = 0;
+  state2 = 0;
+  old_state2 = 0;
+  subiendo = false;
+  bajando = false;
 }
 void reconnect() {
 
-  while (!client.connected()) {
+  while ((!client.connected()) && (flancomqtt == true)) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
     if (client.connect(CLIENT_ID, mqttusuario, mqttpass)) {
       Serial.println("connected");
+      flancomqtt = false;
       delay(20);
       client.subscribe(MQTT_TOPIC "/comando");
       client.subscribe(MQTT_TOPIC "/set_position");
       client.subscribe(MQTT_TOPIC "/position");
 
-
-    } else {
+    } else if (flancomqtt == true) {
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
+      Serial.println(" try again in 5 seconds");  // intenamos conectar al servidos MQTT durante 5 segundos.
 
       for (int i = 0; i <= 500; i++) {
         delay(10);
-        if (i == 50) {
-          Serial.println(".");
-        } else {
-          Serial.print(".");
+        if (i == 500) {
+          cont = 0;
+          flancowifi = false;
+          flancomqtt = false;
+          contador = true;
+          break;
         }
       }
     }
